f0a6fafea8d582fb1cfd2806c1adb135530b701b
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2024-11-07 22:16:09 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# --------------------------- Homework_6.1  ------------------------------------
+"""
+Виконав: Григорій Чернолуцький
+Homework_8
+
+Package Version
+------- -------
+pip 24.3.1
+
+"""
+
+from multilang_calendar import get_data
+
+def main() -> None:
+    # ------------------------  Демо календар ----------------------------
+    print(get_data())
+
+
+
+# --------------------------------- main module ----------------------------------------------
+if __name__ == '__main__':
+    main()
+
+''' 
+РЕЗУЛЬТАТ
+
+['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+'''
